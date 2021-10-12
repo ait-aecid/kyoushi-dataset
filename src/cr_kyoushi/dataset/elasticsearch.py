011fe1f7534fc1eaab5206e7392b4fb37e1199fd
Maximilian Frank <maximilian.frank@ait.ac.at>
2021-10-12 12:55:39 +0200

    Add function docs for elasticsearch util funcs

--- src/cr_kyoushi/dataset/elasticsearch.py
+++ src/cr_kyoushi/dataset/elasticsearch.py
@@ -15,6 +15,25 @@ from elasticsearch_dsl.response.aggs import Bucket
 
 
 def get_transport_variables(es: Elasticsearch) -> Dict[str, Any]:
+    """Utility function for getting Elasticsearch connection info.
+
+    This function takes an Elasticsearch client object and
+    extracts the host connection info and returns it in a dict.
+
+    Args:
+        es: Elasticsearch client object
+
+    Raises:
+        TypeError: If the passed client object is not connected to a host
+
+    Returns:
+        Host connection information dict containing the following fields:
+          - `ELASTICSEARCH_HOST`: The host IP/FQDN and port
+          - `ELASTICSEARCH_HOST_PATH`: The HTTP path if it is part of the full address
+          - `ELASTICSEARCH_SSL`: Boolean indicating SSL on or off
+          - `ELASTICSEARCH_USER`: The username if HTTP auth is used
+          - `ELASTICSEARCH_PASSWORD`: The password if HTTP auth is used
+    """
     if es.transport.hosts is not None and len(es.transport.hosts) > 0:
         # get the first host
         host = es.transport.hosts[0]
@@ -39,6 +58,15 @@ def get_transport_variables(es: Elasticsearch) -> Dict[str, Any]:
 
 
 def scan_composite(search: Search, name: str) -> List[Bucket]:
+    """Utility function for getting all result buckets of a composite aggregate.
+
+    Args:
+        search: The composite aggregate search object.
+        name: The name of the aggregate
+
+    Returns:
+        List of result buckets
+    """
     # ensure that we do not get documents for no reason
     search = search.extra(size=0)
     buckets = []
@@ -60,6 +88,17 @@ def search_eql(
     body: Dict[str, Any],
     check_interval: float = 0.5,
 ) -> Dict[str, Any]:
+    """Utility function for issueing and receiving the result of a EQL query.
+
+    Args:
+        es: The Elasticsearch client object
+        index: The indices to search on
+        body: The EQL query body
+        check_interval: The time to wait in between query ready checks.
+
+    Returns:
+        The EQL query result
+    """
     result = es.eql.search(index=index, body=body, wait_for_completion_timeout="0s")
     result_id = None
     while result["is_running"]:
